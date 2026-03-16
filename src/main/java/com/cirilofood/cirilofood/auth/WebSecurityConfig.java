5ec471f12221cdd87b4d8cc4e18a4fbe56feaf70
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2026-03-16 14:31:38 -0300

    Configuring resource server

--- src/main/java/com/cirilofood/cirilofood/auth/WebSecurityConfig.java
+++ src/main/java/com/cirilofood/cirilofood/auth/WebSecurityConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,18 +16,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("cirilo")
+                    .withUser("cirilo")
                     .password(passwordEncoder().encode("123456"))
                     .roles("ADMIN")
                 .and()
-                .withUser("cirila")
+                    .withUser("cirila")
                     .password(passwordEncoder().encode("654321"))
                     .roles("ADMIN");
 
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
