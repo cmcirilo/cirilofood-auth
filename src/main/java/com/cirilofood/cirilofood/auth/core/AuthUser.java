09fdab0cfd097576d542f8a81941e683a9dc33d6
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2026-03-21 20:07:37 -0300

    Add custom claims in jwt payload

--- src/main/java/com/cirilofood/cirilofood/auth/core/AuthUser.java
+++ src/main/java/com/cirilofood/cirilofood/auth/core/AuthUser.java
@@ -1,18 +1,22 @@
 package com.cirilofood.cirilofood.auth.core;
 
 import com.cirilofood.cirilofood.auth.domain.User;
+import lombok.Getter;
 
 import java.util.Collections;
 
+@Getter
 public class AuthUser extends org.springframework.security.core.userdetails.User {
 
     private static final long serialVersionUID = 1L;
 
+    private Long userId;
     private String fullName;
 
     public AuthUser(User user){
         super(user.getEmail(), user.getPassword(), Collections.emptyList());
 
+        this.userId = user.getId();
         this.fullName = user.getName();
     }
 }
